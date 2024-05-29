e3f849ccf716bf92f200977de4542d5cacd2ea5d
betty2310 <huynh.dh2310@gmail.com>
2024-05-30 00:12:39 +0700

    refactor: remove primsa

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,7 +5,6 @@
 import { getDateCompare } from './date';
 import { fetcher } from './fetcher';
 import { logError, logInfo } from './logger';
-import { prismaDB } from './prismaDB';
 import { displayNumbers } from './utils';
 
-export { displayNumbers, fetcher, getDateCompare, logError, logInfo, prismaDB };
+export { displayNumbers, fetcher, getDateCompare, logError, logInfo };
