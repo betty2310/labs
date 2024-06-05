3996da19c5fc0fe4bd9fc3e418641dc7de5aca51
huydt84 <viethuy061002@gmail.com>
2024-06-05 15:42:59 +0700

    add teacher detail api

---
+++ src/app/api/lab/[id]/teacher/route.ts
@@ -0,0 +1,28 @@
+// app/api/users/[id]/route.ts
+
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+  const { id } = params;
+
+  // Fetch user data from a database or another source
+  const user = await getUserById(id); // This is a placeholder function
+
+  if (user) {
+    return NextResponse.json(user);
+  } else {
+    return NextResponse.json({ message: `User with id: ${id} not found` }, { status: 404 });
+  }
+}
+
+// Placeholder function for data fetching
+async function getUserById(id: string) {
+  // Replace with your actual data fetching logic
+  const users = [
+    { id: '1', name: 'John Doe', "email": "john.doe@gmail.com", "phone": "0912365985", "topic": ["detect student", "build data system"] },
+    { id: '2', name: 'Jane Doe', "email": "jane.doe@gmail.com", "phone": "0912365985", "topic": ["detect student", "build data system"] },
+    { id: '3', name: 'Peter Doe', "email": "peter.doe@gmail.com", "phone": "0912365985", "topic": ["detect student", "build data system"] },
+    { id: '4', name: 'Mary Doe', "email": "mary.doe@gmail.com", "phone": "0912365985", "topic": ["detect student", "build data system"] },
+  ];
+  return users.find(user => user.id === id) || null;
+}
