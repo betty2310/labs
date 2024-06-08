3704a86cae87ff12c125b424307f9039c8b5da5a
dungnn1002 <ngocdungbkhn@gmail.com>
2024-06-08 13:36:33 +0700

    fix eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import MainLayout from '@/layouts/MainLayout/MainLayout';
 import {
   Select,
@@ -9,86 +10,78 @@ import {
 } from '@/components/ui/select';
 import { Input } from '@/components/ui/input';
 import ItemLab from '@/components/ItemLab';
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
-const labs = [
-  {
-    id: '1',
-    name: 'In labs',
-    teacher: 'Nguyễn Văn A',
-    number_of_students: 20,
-    image_urls:
-      'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEje0t9MqvOy7ydtuiyFNrJjG6R52EwYP1FmY4Qr7zzTIcOL7OsLsp_2QVu8k4_L9YCNajRppCinGOIRfwlDkAmwX-jtZQg5zgTQ7IkMdCIJdO0_iw6J1immMIGofnio9CA9rRNF-wRK8qPzHqKoYBQQliWhofsUsN8yrDdg4fmwz9o48yCWGZnQ4bLUqY8/w0/lo-trinh-hoc-thiet-ke-vi-mach-dien-tu-cho-nguoi-moi-bat-dau.jpg',
-    last_updated: '1 hour ago',
-    is_open: true,
-  },
-  {
-    id: '2',
-    name: 'Lab 2',
-    teacher: 'Nguyễn Văn B',
-    number_of_students: 20,
-    image_urls:
-      'https://duhoctrawise.edu.vn/wp-content/uploads/2023/06/1-33.jpg',
-    last_updated: '1 hour ago',
-    is_open: true,
-  },
-  {
-    id: '3',
-    name: 'Lab 3',
-    teacher: 'Nguyễn Văn C',
-    number_of_students: 20,
-    image_urls:
-      'https://cdn.tgdd.vn/hoi-dap/1322322/cam-bien-thong-minh-la-gi-co-nhung-loai-nao-ung-dung-ra-sao%20(3).jpg',
-    last_updated: '1 hour ago',
-    is_open: true,
-  },
-  {
-    id: '1',
-    name: 'In labs',
-    teacher: 'Nguyễn Văn A',
-    number_of_students: 20,
-    image_urls:
-      'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEje0t9MqvOy7ydtuiyFNrJjG6R52EwYP1FmY4Qr7zzTIcOL7OsLsp_2QVu8k4_L9YCNajRppCinGOIRfwlDkAmwX-jtZQg5zgTQ7IkMdCIJdO0_iw6J1immMIGofnio9CA9rRNF-wRK8qPzHqKoYBQQliWhofsUsN8yrDdg4fmwz9o48yCWGZnQ4bLUqY8/w0/lo-trinh-hoc-thiet-ke-vi-mach-dien-tu-cho-nguoi-moi-bat-dau.jpg',
-    last_updated: '1 hour ago',
-    is_open: true,
-  },
-  {
-    id: '2',
-    name: 'Lab 2',
-    teacher: 'Nguyễn Văn B',
-    number_of_students: 20,
-    image_urls:
-      'https://duhoctrawise.edu.vn/wp-content/uploads/2023/06/1-33.jpg',
-    last_updated: '1 hour ago',
-    is_open: true,
-  },
-  {
-    id: '3',
-    name: 'Lab 3',
-    teacher: 'Nguyễn Văn C',
-    number_of_students: 20,
-    image_urls:
-      'https://cdn.tgdd.vn/hoi-dap/1322322/cam-bien-thong-minh-la-gi-co-nhung-loai-nao-ung-dung-ra-sao%20(3).jpg',
-    last_updated: '1 hour ago',
-    is_open: false,
-  },
-];
+
 type Lab = {
   id: string;
   name: string;
   teacher: string;
-  number_of_students: number;
-  last_updated: string;
-  image_urls: string;
-  is_open: boolean;
+  numberOfStudents: number;
+  lastUpdated: string;
+  imageUrls: string;
+  isOpen: boolean;
 };
-
-const getServerSideProps = (async () => {
-  // Fetch data from external API
-  const res = await fetch('https://api.github.com/repos/vercel/next.js');
-  const labs: Lab[] = await res.json();
-  // Pass data to the page via props
-  return { props: { labs } };
-}) satisfies GetServerSideProps<{ labs: Lab[] }>;
+const labs: Lab[] = [
+  {
+    id: '1',
+    name: 'In labs',
+    teacher: 'Nguyễn Văn A',
+    numberOfStudents: 20,
+    imageUrls:
+      'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEje0t9MqvOy7ydtuiyFNrJjG6R52EwYP1FmY4Qr7zzTIcOL7OsLsp_2QVu8k4_L9YCNajRppCinGOIRfwlDkAmwX-jtZQg5zgTQ7IkMdCIJdO0_iw6J1immMIGofnio9CA9rRNF-wRK8qPzHqKoYBQQliWhofsUsN8yrDdg4fmwz9o48yCWGZnQ4bLUqY8/w0/lo-trinh-hoc-thiet-ke-vi-mach-dien-tu-cho-nguoi-moi-bat-dau.jpg',
+    lastUpdated: '1 hour ago',
+    isOpen: true,
+  },
+  {
+    id: '2',
+    name: 'Lab 2',
+    teacher: 'Nguyễn Văn B',
+    numberOfStudents: 20,
+    imageUrls:
+      'https://duhoctrawise.edu.vn/wp-content/uploads/2023/06/1-33.jpg',
+    lastUpdated: '1 hour ago',
+    isOpen: true,
+  },
+  {
+    id: '3',
+    name: 'Lab 3',
+    teacher: 'Nguyễn Văn C',
+    numberOfStudents: 20,
+    imageUrls:
+      'https://cdn.tgdd.vn/hoi-dap/1322322/cam-bien-thong-minh-la-gi-co-nhung-loai-nao-ung-dung-ra-sao%20(3).jpg',
+    lastUpdated: '1 hour ago',
+    isOpen: true,
+  },
+  {
+    id: '1',
+    name: 'In labs',
+    teacher: 'Nguyễn Văn A',
+    numberOfStudents: 20,
+    imageUrls:
+      'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEje0t9MqvOy7ydtuiyFNrJjG6R52EwYP1FmY4Qr7zzTIcOL7OsLsp_2QVu8k4_L9YCNajRppCinGOIRfwlDkAmwX-jtZQg5zgTQ7IkMdCIJdO0_iw6J1immMIGofnio9CA9rRNF-wRK8qPzHqKoYBQQliWhofsUsN8yrDdg4fmwz9o48yCWGZnQ4bLUqY8/w0/lo-trinh-hoc-thiet-ke-vi-mach-dien-tu-cho-nguoi-moi-bat-dau.jpg',
+    lastUpdated: '1 hour ago',
+    isOpen: true,
+  },
+  {
+    id: '2',
+    name: 'Lab 2',
+    teacher: 'Nguyễn Văn B',
+    numberOfStudents: 20,
+    imageUrls:
+      'https://duhoctrawise.edu.vn/wp-content/uploads/2023/06/1-33.jpg',
+    lastUpdated: '1 hour ago',
+    isOpen: true,
+  },
+  {
+    id: '3',
+    name: 'Lab 3',
+    teacher: 'Nguyễn Văn C',
+    numberOfStudents: 20,
+    imageUrls:
+      'https://cdn.tgdd.vn/hoi-dap/1322322/cam-bien-thong-minh-la-gi-co-nhung-loai-nao-ung-dung-ra-sao%20(3).jpg',
+    lastUpdated: '1 hour ago',
+    isOpen: false,
+  },
+];
 
 export default function LabOverview() {
   return (
@@ -113,12 +106,12 @@ export default function LabOverview() {
               {labs.map(lab => (
                 <ItemLab
                   key={lab.id}
-                  is_open={lab.is_open}
+                  isOpen={lab.isOpen}
                   name={lab.name}
                   teacher={lab.teacher}
-                  number_of_students={lab.number_of_students}
-                  last_updated={lab.last_updated}
-                  image_urls={lab.image_urls}
+                  numberOfStudents={lab.numberOfStudents}
+                  lastUpdated={lab.lastUpdated}
+                  imageUrls={lab.imageUrls}
                 />
               ))}
             </div>
--- src/components/ItemLab/index.tsx
+++ src/components/ItemLab/index.tsx
@@ -1,39 +1,41 @@
 'use client';
 
+import Image from 'next/image';
+
 interface LabProps {
-  is_open: boolean;
+  isOpen: boolean;
   name: string;
   teacher: string;
-  number_of_students: number;
-  last_updated: string;
-  image_urls?: string;
+  numberOfStudents: number;
+  lastUpdated: string;
+  imageUrls: string;
 }
 
 const ItemLab: React.FC<LabProps> = ({
-  is_open,
+  isOpen,
   name,
   teacher,
-  number_of_students,
-  last_updated,
-  image_urls,
+  numberOfStudents,
+  lastUpdated,
+  imageUrls,
 }) => {
   return (
     <div className="relative flex flex-col bg-white shadow-md rounded-md p-4">
       <div className="flex justify-between">
         <div className="font-semibold text-lg">{name}</div>
-        <div className="text-sm font-medium">Update {last_updated}</div>
+        <div className="text-sm font-medium">Update {lastUpdated}</div>
       </div>
       <div className="relative mt-2">
-        <img src={image_urls} alt="lab" className="rounded-md h-40 w-full" />
+        <Image src={imageUrls} alt="lab" className="rounded-md h-40 w-full" />
         <div
-          className={`absolute top-0 right-0 p-1 m-2 rounded-md text-white font-bold ${is_open ? 'bg-green-500' : 'bg-red-500'}`}
+          className={`absolute top-0 right-0 p-1 m-2 rounded-md text-white font-bold ${isOpen ? 'bg-green-500' : 'bg-red-500'}`}
         >
-          {is_open ? 'Open' : 'Close'}
+          {isOpen ? 'Open' : 'Close'}
         </div>
       </div>
       <div className="mt-2">
         <div className="text-sm mb-1">Teacher: {teacher}</div>
-        <div className="text-sm">Number of students: {number_of_students}</div>
+        <div className="text-sm">Number of students: {numberOfStudents}</div>
       </div>
     </div>
   );
