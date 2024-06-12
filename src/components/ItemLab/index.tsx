eb941ee7d5cb8883fa504b89c7dd444ae516a316
dungnn1002 <ngocdungbkhn@gmail.com>
2024-06-12 12:38:22 +0700

    fuxk

--- src/components/ItemLab/index.tsx
+++ src/components/ItemLab/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Captions, UsersRound } from 'lucide-react';
-
+import Link from 'next/link';
 import {
   Card,
   CardContent,
@@ -22,6 +22,7 @@ interface LabProps {
   lastUpdated: string;
   imageUrls?: string;
   specialized?: string;
+  id: string;
 }
 
 const ItemLab: React.FC<LabProps> = ({
@@ -33,57 +34,61 @@ const ItemLab: React.FC<LabProps> = ({
   lastUpdated,
   imageUrls,
   specialized,
+  id,
 }) => {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>
-          <div className="flex items-center justify-between">
-            <div className="text-3xl">{name}</div>
-            <Badge variant="secondary">{lastUpdated}</Badge>
+    <Link href={`/labs/${id}`}>
+      {' '}
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            <div className="flex items-center justify-between">
+              <div className="text-3xl">{name}</div>
+              <Badge variant="secondary">{lastUpdated}</Badge>
+            </div>
+            <CardDescription>{specialized}</CardDescription>
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="relative">
+          <img src={imageUrls} alt="lab" className="rounded-md h-40 w-full" />
+          <div className="absolute top-4 right-8">
+            <Badge variant={isOpen ? 'default' : 'destructive'}>
+              {' '}
+              {isOpen ? 'Đang mở' : 'Đã đóng'}
+            </Badge>
           </div>
-          <CardDescription>{specialized}</CardDescription>
-        </CardTitle>
-      </CardHeader>
-      <CardContent className="relative">
-        <img src={imageUrls} alt="lab" className="rounded-md h-40 w-full" />
-        <div className="absolute top-4 right-8">
-          <Badge variant={isOpen ? 'default' : 'destructive'}>
-            {' '}
-            {isOpen ? 'Đang mở' : 'Đã đóng'}
-          </Badge>
-        </div>
-      </CardContent>
-      <CardFooter>
-        <div>
+        </CardContent>
+        <CardFooter>
           <div>
+            <div>
+              <Button
+                variant="outline"
+                className="mb-1 py-1 px-2 text-xs mr-8"
+                size="sm"
+              >
+                <UsersRound className="mr-2 h-4 w-4" /> Giảng viên:{' '}
+                {numberOfteacher}
+              </Button>
+              <Button
+                variant="outline"
+                className="mb-2  py-1 px-2 text-xs"
+                size="sm"
+              >
+                <Captions className="mr-2 h-4 w-4" /> Chủ đề: {numberOftopic}
+              </Button>
+            </div>
             <Button
               variant="outline"
-              className="mb-1 py-1 px-2 text-xs mr-8"
+              className="mb-1  py-1 px-2 text-xs"
               size="sm"
             >
-              <UsersRound className="mr-2 h-4 w-4" /> Giảng viên:{' '}
-              {numberOfteacher}
-            </Button>
-            <Button
-              variant="outline"
-              className="mb-2  py-1 px-2 text-xs"
-              size="sm"
-            >
-              <Captions className="mr-2 h-4 w-4" /> Chủ đề: {numberOftopic}
+              <UsersRound className="mr-2 h-4 w-4" /> Sinh viên:{' '}
+              {numberOfStudents}
             </Button>
           </div>
-          <Button
-            variant="outline"
-            className="mb-1  py-1 px-2 text-xs"
-            size="sm"
-          >
-            <UsersRound className="mr-2 h-4 w-4" /> Sinh viên:{' '}
-            {numberOfStudents}
-          </Button>
-        </div>
-      </CardFooter>
-    </Card>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 };
 
